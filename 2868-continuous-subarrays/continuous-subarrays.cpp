215a3c4b231a7530394e64a61e646cc7f8db92c9
Kuldeep Kumar <117506995+Kuldeep8081@users.noreply.github.com>
2024-12-14 21:24:45 +0530

    Time: 158 ms (47.31%) | Memory: 129.7 MB (39.50%) - LeetSync

---
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    #define P pair<int,int>
+    long long continuousSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        priority_queue<P,vector<P>,greater<P>>minHeap;
+        priority_queue<P>maxHeap;
+
+        int i=0;
+        int j=0;
+        long long count=0;
+
+        while(j<n){
+            minHeap.push({nums[j],j});
+            maxHeap.push({nums[j],j});
+
+            while(abs(maxHeap.top().first-minHeap.top().first)>2){
+                i++;
+                while(!maxHeap.empty() && maxHeap.top().second<i){
+                    maxHeap.pop();
+                }
+
+                while(!minHeap.empty() && minHeap.top().second<i){
+                    minHeap.pop();
+                }
+            }
+            count+=j-i+1;
+            j++;
+        }
+
+        return count;
+    }
+};
