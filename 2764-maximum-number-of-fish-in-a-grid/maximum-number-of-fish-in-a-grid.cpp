6bce617fdeb0d3914fcfc107bde376f3c77aa09b
Kuldeep Kumar <117506995+Kuldeep8081@users.noreply.github.com>
2025-01-28 12:04:54 +0530

    Time: 41 ms (11.73%) | Memory: 114.4 MB (7.88%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    vector<pair<int,int>>dir={{1,0},{0,1},{0,-1},{-1,0}};
+    int findMaxFish(vector<vector<int>>& grid) {
+        
+        int m=grid.size();
+        int n=grid[0].size();
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                
+                if(grid[i][j]>0){
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    int sum=0;
+                    while(!q.empty()){
+                        pair<int,int>p=q.front();
+                        q.pop();
+                        int i_=p.first;
+                        int j_=p.second;
+                        sum+=grid[i_][j_];
+                        grid[i_][j_]=0;
+                        for(auto pa : dir){
+                            int newi=i_+pa.first;
+                            int newj=j_+pa.second;
+
+                            if(newi>=0 && newi<m && newj>=0 && newj<n && grid[newi][newj]>0){
+                                q.push({newi,newj});
+                            }
+                        }
+                    }
+
+                    ans=max(ans,sum);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
