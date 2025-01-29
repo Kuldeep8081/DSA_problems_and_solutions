1723699725212743292401fb74267ae59407c694
Kuldeep Kumar <117506995+Kuldeep8081@users.noreply.github.com>
2025-01-29 22:37:07 +0530

    Time: 25 ms (6.62%) | Memory: 18 MB (9.38%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        
+
+        unordered_map<int,vector<int>>adj;
+
+        for(auto vec : edges){
+            int val1=vec[0];
+            int val2=vec[1];
+
+            adj[val1].push_back(val2);
+            adj[val2].push_back(val1);
+        }
+        
+        vector<int>ans;
+        for(int i=edges.size()-1;i>=0;i--){
+            auto vec=edges[i];
+            int val1=vec[0];
+            int val2=vec[1];
+            unordered_map<int,bool>nodeVisited;
+
+            auto vec1=adj[val1];
+            
+            nodeVisited[val1]=true;
+
+            stack<int>st;
+            
+            for(int i=0;i<vec1.size();i++){
+                if(vec1[i]!=val2)
+                st.push(vec1[i]);
+            }
+
+            while(!st.empty()){
+                int val=st.top();
+                st.pop();
+                nodeVisited[val]=true;
+                auto vec2=adj[val];
+                
+                if(val2==val){
+                    ans={val1,val2};
+                    return ans;
+                }
+                for(int i=0;i<vec2.size();i++){
+                    if(!nodeVisited[vec2[i]]){
+                        st.push(vec2[i]);
+                    }
+                }
+            }
+
+            
+        }
+
+        return ans;
+    }
+};
