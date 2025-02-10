ea6c717d93eb8df56700d8aab3e1a253fd295698
Kuldeep Kumar <117506995+Kuldeep8081@users.noreply.github.com>
2025-02-10 09:56:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (87.14%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     string clearDigits(string s) {
         
+        string st="";
         for(int i=0;i<s.length();i++){
             
-            int digit=s[i]-'0';
-            if(digit>=0 && digit<=9){
-                if(i>0)
-                {
-                    s.erase(s.begin()+i-1,s.begin()+i+1);
-                    i-=2;
-                }else{
-                    s.erase(s.begin()+i);
-                    i--;
-                }
+            int val=s[i]-'0';
 
-                
+            if(val>=0 && val<=9 && st.size()){
+                st.pop_back();
             }
+            else
+            st.push_back(s[i]);
         }
 
-        return s;
+        return st;
     }
 };
