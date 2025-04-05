c10d6a06efd36e098751d6691101fad48f558e37
Kuldeep Kumar <117506995+Kuldeep8081@users.noreply.github.com>
2025-04-05 22:29:09 +0530

    Time: 3 ms (39.68%) | Memory: 9.1 MB (90.00%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        
+        int n=nums.size();
+        int totalXORSum=0;
+
+        int totalSubsets=1<<n;
+
+        for(int i=0;i<totalSubsets;i++){
+            int currentXOR=0;
+
+            for(int j=0;j<n;j++){
+
+                if(i&(1<<j)){
+                    currentXOR^=nums[j];
+                }
+            }
+
+            totalXORSum+=currentXOR;
+        }
+
+        return totalXORSum;
+    }
+};
